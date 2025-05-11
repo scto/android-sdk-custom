49e53e4393c55a0074d279b310de1141b53d6e71
Homura <egzozu.be1.bas@gmail.com>
2025-05-11 17:57:24 +0300

    Update sqlite3.cmake

--- platform-tools/sqlite3.cmake
+++ platform-tools/sqlite3.cmake
@@ -48,4 +48,4 @@ target_compile_definitions(sqlite3 PRIVATE
     -DSQLITE_ENABLE_DBSTAT_VTAB
     -DNO_ANDROID_FUNCS=1
     )
-target_link_libraries(sqlite3 dl z)
+target_link_libraries(sqlite3 dl ${CMAKE_PREFIX_PATH}/lib/libz.a)
