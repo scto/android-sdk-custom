4e2180a8556415f3254e34409e11b05ed17fa575
Homura <egzozu.be1.bas@gmail.com>
2025-05-11 17:35:43 +0300

    Update faked_functions.cpp

--- src/faked_functions.cpp
+++ src/faked_functions.cpp
@@ -1,3 +1,5 @@
+#include "faked_functions.h"
+
 #include <cstdint>
 #include <cstring>
 #include <string>
@@ -7,30 +9,26 @@
 #define MAX_NAME_LEN   64
 #define MAX_VALUE_LEN  256
 
-struct prop_info {
-    char name[MAX_NAME_LEN];
-    char value[MAX_VALUE_LEN];
-    uint32_t serial;
-};
-
 static prop_info property_store[MAX_PROPERTIES];
 static size_t property_count = 0;
 static std::mutex property_mutex;
 
-inline int __system_property_foreach(void (*propfn)(const prop_info* pi, void* cookie),
-                                     void* cookie) {
-    std::lock_guard<std::mutex> lock(property_mutex);
+extern "C" {
+    int __system_property_foreach(void (*propfn)(const prop_info* pi, void* cookie),
+                                   void* cookie) {
+        std::lock_guard<std::mutex> lock(property_mutex);
 
-    for (size_t i = 0; i < property_count; ++i) {
-        propfn(&property_store[i], cookie);
+        for (size_t i = 0; i < property_count; ++i) {
+            propfn(&property_store[i], cookie);
+        }
+        return 0;
     }
-    return 0;
-}
 
-inline void __system_property_read_callback(
-    const prop_info* pi,
-    void (*callback)(void* cookie, const char* name, const char* value, uint32_t serial),
-    void* cookie) {
-    
-    callback(cookie, pi->name, pi->value, pi->serial);
+    void __system_property_read_callback(
+        const prop_info* pi,
+        void (*callback)(void* cookie, const char* name, const char* value, uint32_t serial),
+        void* cookie) {
+
+        callback(cookie, pi->name, pi->value, pi->serial);
+    }
 }
